5710db8d2285e21e14ce829e27121ca5a43d4d71
Jason Hamren <jason@hoodoo.digital>
2024-06-04 16:44:41 -0600

    flip card test

---
+++ blocks/flip-card/flip-card.js
@@ -0,0 +1,28 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  /* change to ul, li */
+  const ul = document.createElement('ul');
+  [...block.children].forEach((row) => {
+    const li = document.createElement('li');
+    const wrapper = document.createElement('div');
+    wrapper.className = 'content';
+    li.append(wrapper);
+    li.className = 'flippable-card';
+    moveInstrumentation(row, wrapper);
+    while (row.firstElementChild) wrapper.append(row.firstElementChild);
+    [...wrapper.children].forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else div.className = 'cards-card-body';
+    });
+    ul.append(li);
+  });
+  ul.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
+  block.textContent = '';
+  block.append(ul);
+}
