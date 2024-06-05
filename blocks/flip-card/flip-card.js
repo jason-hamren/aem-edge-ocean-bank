2abf458aad0c896150261508cb06c935bb7090c8
Jason Hamren <jason@hoodoo.digital>
2024-06-05 09:19:18 -0600

    flip card updates

---
+++ blocks/flip-card/flip-card.js
@@ -0,0 +1,16 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  [...block.children].forEach((div) => {
+    if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+    else div.className = 'cards-card-body';
+  });
+
+  block.querySelectorAll('picture > img').forEach((img) => {
+    const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
+    moveInstrumentation(img, optimizedPic.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPic);
+  });
+  block.textContent = '';
+}
