aa1316a9b8cb76a184cb299770240103e6d4b447
nishawilliams <nishawilliams@rightpoint.com>
2024-06-10 18:17:16 -0600

    OF-teaser - teaser highlight

--- blocks/highlight/highlight.js
+++ blocks/highlight/highlight.js
@@ -1,5 +1,16 @@
 export default function decorate(block) {
   const [h, nh] = block.children;
-  h.className = 'highlighted';
+  h.className = 'highlight-section';
   nh.className = 'nohighlight';
+
+  const highlightContainer = block.firstChild();
+  const span = document.createElement('span');
+  span.className = 'highlighted';
+  const highlightHeader = highlightContainer.querySelector('h2');
+  const headerText = highlightHeader.innerHTML;
+  const headerArray = headerText.split(' ');
+  const highlightedText = headerArray.slice(0, 2).join(' ');
+  span.innerHTML = highlightedText;
+  highlightHeader.innerHTML = ` ${headerArray.slice(2).join(' ')}`;
+  highlightHeader.prepend(span);
 }
