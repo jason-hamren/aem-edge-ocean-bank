4de6052fcc049caa36bf20672bd6a37f6cfab9d6
nishawilliams <nishawilliams@rightpoint.com>
2024-06-20 14:13:50 -0600

    OF-statictitlecards - making conatiner for cards.

--- blocks/statictitlecards/statictitlecards.js
+++ blocks/statictitlecards/statictitlecards.js
@@ -1,4 +1,20 @@
+/**
+ * Wrap an array of items all at once
+ */
+function wrapAll(children, wrapper) {
+  if (children && children.length) {
+    children[0].parentNode.insertBefore(wrapper, children[0]);
+    [...children].forEach((child) => {
+      if (child === wrapper) return;
+      wrapper.appendChild(child);
+    });
+  }
+}
+
 export default function decorate(block) {
   const [card] = block.children;
   card.className = 'card';
+  const cardContainer = document.createElement('div');
+  cardContainer.className = 'card-container';
+  wrapAll(block.querySelectorAll("[data-aue-label='Statictitlecard']"), cardContainer);
 }
