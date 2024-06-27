8d8da4626ed736747d544560f079a4945d3e5e14
Jason Hamren <jason@hoodoo.digital>
2024-06-27 13:49:34 -0600

    testing

--- blocks/templateblock/templateblock.js
+++ blocks/templateblock/templateblock.js
@@ -1,28 +1,5 @@
 export default function decorate(block) {
-  const [title, description] = block.children;
-  const wrapper = document.createElement('div');
-  wrapper.className = 'templateblock-wrapper-custom';
-  wrapper.append(...block.children);
-  console.log('This is running');
-
-  const baseHtml = `<div class="ofs-flippable-card">
-                      <div class="ofs-card-side-front">
-                       <h2 class="content-title">${title}</h2>
-                       <div class="action-container">
-                        <button class="action"> <span class="action-icon"></span></button>
-                       </div>
-                      </div>   
-                      <div class="ofs-card-side-back">
-                       <div class="content-description">
-                        <h2 class="content-title">${title}</h2>
-                        <p class="content-info">${description}</p>
-                        <a class="action-link" href="https://www.cognitoforms.com/OceanBank2/OceanFinancialServicesContactMe">Let's connect</a>    
-                       </div>
-                       <div class="action-container">
-                        <button class="action"><span class="action-icon"></span></button>
-                       </div>
-                      </div>   
-                     </div>`;
-
-  wrapper.append(baseHtml);
+  const [col1, col2] = block.children;
+  col1.className = 'col1';
+  col2.className = 'col2';
 }
