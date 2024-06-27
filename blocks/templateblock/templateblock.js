0eed9d2ab949ae860dfc461334482c9608808bcf
Jason Hamren <jason@hoodoo.digital>
2024-06-27 13:56:43 -0600

    testing

--- blocks/templateblock/templateblock.js
+++ blocks/templateblock/templateblock.js
@@ -1,5 +1,15 @@
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
+  console.log('templateblock');
   const [col1, col2] = block.children;
   col1.className = 'col1';
   col2.className = 'col2';
+  const wrapper = document.createElement('div');
+  wrapper.className = 'templateblock-wrapper-custom';
+  block.prepend(wrapper);
+  [...block.children].forEach((child) => {
+    moveInstrumentation(child, wrapper);
+    while (child.firstElementChild) wrapper.append(child.firstElementChild);
+  });
 }
