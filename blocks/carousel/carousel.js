7060795502ae81aae6ba4564634c911acca99ecd
nishawilliams <nishawilliams@rightpoint.com>
2024-07-05 17:49:10 -0600

    OF-carousel - adding classes to carousel cards.

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,13 +1,9 @@
 /**
  * Wrap an array of items all at once
  */
-function wrapAll(children, wrapper) {
-  if (children && children.length) {
-    children[0].parentNode.insertBefore(wrapper, children[0]);
-    [...children].forEach((child) => {
-      if (child === wrapper) return;
-      wrapper.appendChild(child);
-    });
+function addParentClass(child, className) {
+  if (child && child.length) {
+    child[0].parentNode.classList.add(className);
   }
 }
 
@@ -15,13 +11,9 @@ export default function decorate(block) {
   // add classes
   [...block.children].forEach((carouselItem) => {
     // add class to image
-    const imgContainer = document.createElement('div');
-    imgContainer.className = 'img-container';
     carouselItem.classList.add('carousel-item');
-    wrapAll(carouselItem.querySelector('picture'), imgContainer);
+    addParentClass(carouselItem.querySelector('picture'), 'img-container');
     // add class to content card
-    const contentCard = document.createElement('div');
-    contentCard.className = 'content-card';
-    wrapAll(carouselItem.querySelectorAll("[data-aue-label='Title']"), contentCard);
+    addParentClass(carouselItem.querySelectorAll("[data-aue-label='Title']"), 'content-card');
   });
 }
