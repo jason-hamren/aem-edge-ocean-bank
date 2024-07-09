e76e8603495afa216c8d55ca6f84b7658b80265e
Jason Hamren <jason@hoodoo.digital>
2024-07-09 15:00:24 -0600

    more tests

--- blocks/templateblock/templateblock.js
+++ blocks/templateblock/templateblock.js
@@ -1,19 +1,17 @@
 export default function decorate(block) {
   const [title, description] = [...block.children];
-  block.textContent = '';
   const wrapper = document.createElement('div');
   wrapper.className = 'templateblock-wrapper-custom';
   wrapper.innerHTML = `
     <div class="ofs-flippable-card">
       <div class="ofs-card-side-front">
-        ${title.innerHTML}
+        <div class="content-title"></div>
         <div class="action-container">
           <button class="action"> <span class="action-icon"></span></button>
         </div>
       </div>
       <div class="ofs-card-side-back">
         <div class="content-description">
-          ${description.innerHTML}
           <a class="action-link" href="https://www.cognitoforms.com/OceanBank2/OceanFinancialServicesContactMe">Let's connect</a>
         </div>
         <div class="action-container">
@@ -22,6 +20,10 @@ export default function decorate(block) {
       </div>
     </div>
     `;
-
+  block.textContent = '';
   block.append(wrapper);
+  const titleLocation = wrapper.querySelector('.content-title');
+  const contentDescription = wrapper.querySelector('.content-description');
+  titleLocation.append(title.innerHTML);
+  contentDescription.append(description.innerHTML);
 }
