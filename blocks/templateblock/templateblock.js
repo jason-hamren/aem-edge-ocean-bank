616a3a7478259ea540fa0d93ae46d5c126ee0260
Jason Hamren <jason@hoodoo.digital>
2024-07-09 13:51:45 -0600

    more tests

--- blocks/templateblock/templateblock.js
+++ blocks/templateblock/templateblock.js
@@ -5,14 +5,14 @@ export default function decorate(block) {
   wrapper.innerHTML = `
     <div class="ofs-flippable-card">
       <div class="ofs-card-side-front">
-        ${title.firstElementChild.outerHTML}
+        ${title}
         <div class="action-container">
           <button class="action"> <span class="action-icon"></span></button>
         </div>
       </div>
       <div class="ofs-card-side-back">
         <div class="content-description">
-          ${description.firstElementChild.outerHTML}
+          ${description}
           <a class="action-link" href="https://www.cognitoforms.com/OceanBank2/OceanFinancialServicesContactMe">Let's connect</a>
         </div>
         <div class="action-container">
