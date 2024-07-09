e71ce8dad90215990e5ba9a64edf3baac933a5b9
Jason Hamren <jason@hoodoo.digital>
2024-07-09 16:07:30 -0600

    more tests

--- blocks/templateblock/templateblock.js
+++ blocks/templateblock/templateblock.js
@@ -3,7 +3,7 @@ import templateBlockMarkup from '../../scripts/html/markup-templates.js';
 export default function decorate(block) {
   const [title, description] = block.children;
   const wrapper = document.createElement('div');
-  wrapper.className = 'templateblock-wrapper-custom-jason';
+  wrapper.className = 'templateblock-wrapper-custom';
   wrapper.innerHTML = templateBlockMarkup(title, description);
   block.textContent = '';
   block.append(wrapper);
