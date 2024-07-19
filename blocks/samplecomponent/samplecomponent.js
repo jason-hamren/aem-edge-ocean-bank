f5f40bd5a321db5c742309263b2ee3304477ca4c
Jason Hamren <jason@hoodoo.digital>
2024-07-19 12:47:21 -0600

    fixes for demo

--- blocks/samplecomponent/samplecomponent.js
+++ blocks/samplecomponent/samplecomponent.js
@@ -1,6 +1,12 @@
-export default function decorate(block) {
-  const [txt, desc] = block.children;
+import sampleComponentMarkup from '../../scripts/html/html-templates.js';
 
-  txt.className = 'text-class';
-  desc.className = 'caption';
+export default function decorate(block) {
+  const [title, description, image] = block.children;
+  const wrapper = document.createElement('div');
+  wrapper.className = 'samplecomponent-wrapper-custom';
+  wrapper.innerHTML = sampleComponentMarkup(title, description, image);
+  // explain all the things
+  block.textContent = '';
+  block.append(wrapper);
 }
+
