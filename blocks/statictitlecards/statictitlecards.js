2169c9b46c0aa4a959e80e332557ae105e1576ce
nishawilliams <nishawilliams@rightpoint.com>
2024-07-29 18:44:03 -0600

    OF-style-updates - updating statictitlecards.

--- blocks/statictitlecards/statictitlecards.js
+++ blocks/statictitlecards/statictitlecards.js
@@ -1,14 +1,30 @@
+/**
+ * Wrap an array of items all at once
+ */
+function wrapAll(children, wrapper) {
+  if (children && children.length) {
+    children[0].parentNode.insertBefore(wrapper, children[0]);
+    [...children].forEach((child) => {
+      if (child === wrapper) return;
+      wrapper.appendChild(child);
+    });
+  }
+}
+
 export default function decorate(block) {
+  const cardContainer = document.createElement('div');
+  cardContainer.className = 'card-container';
   let isFirst = true;
   [...block.children].forEach((child) => {
     if (isFirst) {
       child.classList.add('card');
       isFirst = false;
     } else {
-      child.classList.add('card-container');
+      child.classList.add('statictitlecard');
     }
   });
 
+  wrapAll(block.querySelectorAll('.statictitlecard'), cardContainer);
   const staticcards = document.querySelector('.statictitlecards');
   if (staticcards != null) {
     const staticcardsTop = staticcards.offsetTop + 50;
