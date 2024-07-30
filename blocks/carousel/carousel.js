eb85e9f2266c4c0f6f7be5738a6d46327da3ea46
nishawilliams <nishawilliams@rightpoint.com>
2024-07-30 13:09:46 -0600

    OF-carousel-updates - Update template for carousel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -35,6 +35,6 @@ export default function decorate(block) {
     // eslint-disable-next-line max-len
     carouselContainer.append(createCarouselItem(imageContent, cardContent));
   });
-  block.appendChild = carouselContainer;
+  block.appendChild(carouselContainer);
   block.appendChild(createActionContainer());
 }
