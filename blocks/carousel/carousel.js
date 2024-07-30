180661c6d8359f9716df641ab6b050ea2762d72d
nishawilliams <nishawilliams@rightpoint.com>
2024-07-30 14:48:25 -0600

    OF-carousel-updates - Update template for carousel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -36,6 +36,6 @@ export default function decorate(block) {
     carouselContainer.append(createCarouselItem(imageContent, cardContent));
   });
   block.textContent = '';
-  block.insertAdjacentHTML('beforeend', carouselContainer.toString());
+  block.innerHTML =  carouselContainer.toString();
   block.insertAdjacentHTML('beforend', createActionContainer());
 }
