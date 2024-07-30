338f2973dcfe983bebf256adf8c922abcc1e160b
nishawilliams <nishawilliams@rightpoint.com>
2024-07-30 12:37:05 -0600

    OF-carousel-updates - Update template for carousel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,15 +1,19 @@
-function createCarouselItem(img, imgAlt, title, text) {
+function createCarouselItem(imageContent, cardContent) {
+  const imageSrc = imageContent && imageContent.querySelector('img').getAttribute('src');
+  const imgAlt = imageContent && imageContent.querySelector('img').getAttribute('alt');
+  const cardTitle = cardContent && cardContent.querySelector('p:nth-of-type(1)')?.innerText;
+  const cardText = cardContent && cardContent.querySelector('p:nth-of-type(2)')?.innerText;
   return `
     <div class="carousel-item">
        <div class="img-container">
           <picture>
-            <img src="${img}" alt="${imgAlt}">
+            <img src="${imageSrc || ''}" alt="${imgAlt || ''}">
           </picture>
        </div>
        <div class="content-card">
-          <h2 class="content-title">${title}</h2>
+          <h2 class="content-title">${cardTitle || ''}</h2>
           <div class="content-text">
-            <p>${text}</p>
+            <p>${cardText || ''}</p>
           </div>
        </div>
     </div>`;
@@ -27,9 +31,9 @@ export default function decorate(block) {
   // add classes
   const carouselContainer = document.createElement('div');
   [...block.children].forEach((carouselItem) => {
-    const [image, imageAlt, cardTitle, cardText] = carouselItem.children;
+    const [imageContent, cardContent] = carouselItem.children;
     // eslint-disable-next-line max-len
-    carouselContainer.append(createCarouselItem(image, imageAlt, cardTitle, cardText));
+    carouselContainer.append(createCarouselItem(imageContent, cardContent));
   });
 
   block.append(carouselContainer);
