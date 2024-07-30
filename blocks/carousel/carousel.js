93d130f14144ddffe48ab29c5cd7442f4f801462
nishawilliams <nishawilliams@rightpoint.com>
2024-07-30 14:49:27 -0600

    OF-carousel-updates - Update template for carousel

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -19,13 +19,13 @@ function createCarouselItem(imageContent, cardContent) {
     </div>`;
 }
 
-function createActionContainer() {
-  return `
-    <div class="carousel-actions">
-      <button class="action-prev"></button>
-      <button class="action-next"></button>
-    </div>`;
-}
+// function createActionContainer() {
+//   return `
+//     <div class="carousel-actions">
+//       <button class="action-prev"></button>
+//       <button class="action-next"></button>
+//     </div>`;
+// }
 
 export default function decorate(block) {
   // add classes
@@ -37,5 +37,5 @@ export default function decorate(block) {
   });
   block.textContent = '';
   block.innerHTML = carouselContainer.toString();
-  block.insertAdjacentHTML('beforend', createActionContainer());
+  // block.insertAdjacentHTML('beforend', createActionContainer());
 }
