fbdf356703a9a4816bf3db0acc5e79e4c0c82b27
nishawilliams <nishawilliams@rightpoint.com>
2024-08-09 10:46:36 -0600

    OF-carousel-updates- updating wrapper html

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -61,7 +61,7 @@ export default function decorate(block) {
     const [imageContent, cardContent] = carouselItem.children;
     // eslint-disable-next-line max-len
     // carouselContainer.insertAdjacentHTML('beforeend', createCarouselItem(imageContent, cardContent));
-    imageContent.classList.add('img-class');
+    imageContent.classList.add('img-container');
     cardContent.classList.add('content-card');
     if (cardContent && cardContent.children.length) {
       let isFirst = true;
